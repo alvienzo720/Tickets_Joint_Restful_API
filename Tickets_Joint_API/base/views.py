de705018f237ef3b475151e9ca2c364c64ca2469
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2022-10-22 21:25:32 +0300

    Intial Commit on Dev Branch

--- Tickets_Joint_API/base/views.py
+++ Tickets_Joint_API/base/views.py
@@ -8,3 +8,13 @@ from django.http import JsonResponse
 def endpoints(request):
     data = ['/tickets', 'tickets/:ticketname']
     return JsonResponse(data, safe=False)
+
+
+def ticket_list(request):
+    data = ['Nyege-Nyege', 'Bayimba-Festival', 'Roast&Rhyme']
+    return JsonResponse(data, safe=False)
+
+
+def ticket_details(request, ticketname):
+    data = ticketname
+    return JsonResponse(data, safe=False)
