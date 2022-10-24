913a35e8b6431c0bf3a3a2631fc8f107d3163fe6
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2022-10-24 19:03:27 +0300

    Add Ticket Model

--- Tickets_Joint_API/base/views.py
+++ Tickets_Joint_API/base/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Ticket
 # Create your views here.
 
 
-
+@api_view(['GET'])
 def endpoints(request):
     data = ['/tickets', 'tickets/:ticketname']
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
-
+@api_view(['GET'])
 def ticket_list(request):
-    data = ['Nyege-Nyege', 'Bayimba-Festival', 'Roast&Rhyme']
-    return JsonResponse(data, safe=False)
+    tickets = Ticket.objects.all()
+    return Response(tickets)
 
-
+@api_view(['GET'])
 def ticket_details(request, ticketname):
     data = ticketname
-    return JsonResponse(data, safe=False)
+    return Response(data)
