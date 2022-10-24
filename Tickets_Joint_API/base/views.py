41c5ba80e71bd94f5c04480e528da879b627cb88
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2022-10-24 20:19:50 +0300

    Add CRUD to Tickets views

--- Tickets_Joint_API/base/views.py
+++ Tickets_Joint_API/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,18 +12,49 @@ def endpoints(request):
     data = ['/tickets', 'tickets/:ticketname']
     return Response(data)
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def ticket_list(request):
-    query = request.GET.get('query')
-    
-    if query == None:
-        query = ''
-    tickets = Ticket.objects.filter(ticket_name__icontains=query)
-    serializer = TicektSerializer(tickets, many=True)
-    return Response(serializer.data)
+    # handles GET requests
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        
+        if query == None:
+            query = ''
+        tickets = Ticket.objects.filter(ticket_name__icontains=query)
+        serializer = TicektSerializer(tickets, many=True)
+        return Response(serializer.data)
 
-@api_view(['GET'])
+    if request.method == 'POST':
+        ticket = Ticket.objects.create(
+            ticket_name=request.data['ticket_name'],
+            price=request.data['price'],
+            date=request.data['date'],
+            ticket_owner=request.data['ticket_owner']
+            )
+        serializer = TicektSerializer(ticket, many=False)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def ticket_details(request, ticketname):
     ticket = Ticket.objects.get(ticket_name=ticketname)
-    seralizer = TicektSerializer(ticket, many=False)
-    return Response(seralizer.data)
+    if request.method == 'GET':
+        seralizer = TicektSerializer(ticket, many=False)
+        return Response(seralizer.data)
+
+    if request.method == 'PUT':
+        ticket.ticket_name == request.data['ticket_name']
+        ticket.price = request.data['price']
+        ticket.data = request.data['date']
+        ticket.ticket_owner = request.data['ticket_owner']
+        ticket.save()
+
+        seralizer = TicektSerializer(ticket, many=False)
+        return Response((seralizer.data))
+
+    if request.method == 'DELETE':
+        ticket.delete()
+        return Response('Ticket Deleted')
+
+
+
